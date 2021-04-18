3a3e56239f9b008f932126327d55d7da9e204463
ProCode2 <bpro249@gmail.com>
2021-04-18 14:57:38 +0530

    made a naive repl that prints the tokents generated by the lexer

--- src/repl.rs
+++ src/repl.rs
@@ -1,1 +1,20 @@
-let prompt = ">>"
+use crate::lexer::Lexer;
+use std::io::{stdin, stdout, Write};
+
+pub fn start() {
+  let prompt = ">> ";
+  let mut input = String::new();
+  loop {
+    print!("{}", prompt);
+    stdout().flush().expect("Something went wrong");
+    stdin().read_line(&mut input).unwrap();
+    if input.trim() == "exit" {
+      break;
+    }
+    let mut l = Lexer::new(input.clone());
+    while l.peek_char() != '\0' {
+      println!("{:?}", l.next_token())
+    }
+    input = "".to_string();
+  }
+}
